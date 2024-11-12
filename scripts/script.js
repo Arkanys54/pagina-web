99ee25830cb5c4ac3dc8bbdc1ca6a27088973c1e
Arkanys85 <juansebaslc54@gmail.com>
2024-11-12 17:41:36 -0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+// Carrito de compras
+let cart = [];
+
+function addToCart(productName) {
+    cart.push(productName);
+    alert(productName + " se ha añadido al carrito.");
+    updateCart();
+}
+
+function updateCart() {
+    const cartItems = document.getElementById("cartItems");
+    cartItems.innerHTML = ""; // Limpia el carrito antes de actualizar
+    cart.forEach((item, index) => {
+        const cartItem = document.createElement("div");
+        cartItem.textContent = item;
+        cartItems.appendChild(cartItem);
+    });
+}
+
+function checkout() {
+    if (cart.length > 0) {
+        alert("Gracias por su compra. Procesando pago...");
+        cart = []; // Vaciar carrito después de la compra
+        updateCart();
+    } else {
+        alert("Tu carrito está vacío.");
+    }
+}
+
+function proceedToPayment() {
+    window.location.href = "../htmls/pago.html";
+}
+
+
+function verProducto(nombre, descripcion, precio, imagen) {
+    const url = `producto.html?nombre=${encodeURIComponent(nombre)}&descripcion=${encodeURIComponent(descripcion)}&precio=${precio}&imagen=${encodeURIComponent(imagen)}`;
+    window.location.href = url;
+}
